db570a078942f934c97c0e36350bdc80e85879aa
khang <khangcaopham2003@gmail.com>
2024-04-15 09:00:07 +0700

    sửa lỗi vặt

--- BMShop/Controllers/NewController.cs
+++ BMShop/Controllers/NewController.cs
@@ -1,8 +1,4 @@
 ﻿using Model.Dao;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace BMShop.Controllers
--- Model/EF/Category.cs
+++ Model/EF/Category.cs
@@ -1,13 +1,11 @@
 namespace Model.EF
 {
     using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("Category")]
-    public partial class Category
+    public class Category
     {
         public long ID { get; set; }
 
@@ -40,8 +38,8 @@ namespace Model.EF
         [StringLength(250)]
         public string MetaDescriptions { get; set; }
 
-        public bool? Status { get; set; }
+        public bool Status { get; set; }
 
-        public bool? ShowOnHome { get; set; }
+        public bool ShowOnHome { get; set; }
     }
 }
